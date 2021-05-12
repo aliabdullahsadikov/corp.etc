53e24dbb308be10ce99827f7f03b46dcfd4927cb
Aliabdullah <aliabdullahsadikov@mail.ru>
2021-05-12 18:12:15 +0500

    'first'

---
+++ run.py
@@ -0,0 +1,12 @@
+from app import app
+# from app import config
+# from app import models
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    app.run(threaded=True, debug=True)
+
+
+    # models.db.create_all()
+    # models.manager.run()
+    # app.run(debug=True)
